84ff19af3c9b173c0c7318ec56b3f22d1582638e
Alan <alanbritodossantos2015@gmail.com>
2023-05-18 05:46:20 -0300

    Registrando o Contexto do Banco de Dados

--- backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Program.cs
+++ backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Program.cs
@@ -1,4 +1,7 @@
 
+using GerenciamentoTarefasJBS.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace GerenciamentoTarefasJBS
 {
     public class Program
@@ -10,6 +13,11 @@ namespace GerenciamentoTarefasJBS
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
