525ee3064639a7cd9d273495a40c9a4fc27f7b55
Alan <alanbritodossantos2015@gmail.com>
2023-05-18 04:50:23 -0300

    entidades principais da nossa aplicação

---
+++ backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace GerenciamentoTarefasJBS.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciamentoTarefasJBS.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+}
