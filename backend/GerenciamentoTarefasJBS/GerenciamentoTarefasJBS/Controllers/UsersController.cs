05090e2e7da0fc58ec156e85bfb78788427306f7
Alan <alanbritodossantos2015@gmail.com>
2023-05-18 12:49:44 -0300

    foi adicionado uma migration e aplicado o migration para a criação do bando de dados no SQL Server Management Studio (SSMS)

---
+++ backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GerenciamentoTarefasJBS.Data;
+using GerenciamentoTarefasJBS.Models;
+using System.Threading.Tasks;
+
+namespace GerenciamentoTarefasJBS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //a ação GET que retorna todos os usuários
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
