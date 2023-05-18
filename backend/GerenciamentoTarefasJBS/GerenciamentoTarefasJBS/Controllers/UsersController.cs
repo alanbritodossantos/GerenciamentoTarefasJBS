53efdacc1d22416058b1bff51982ad49e3b31cd0
Alan <alanbritodossantos2015@gmail.com>
2023-05-18 13:44:02 -0300

    permitindo que a propriedade Tasks possa ser null na classe users, uma nova migração foi aplicada para a mudança ao banco de dados

--- backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Controllers/UsersController.cs
+++ backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Controllers/UsersController.cs
@@ -24,5 +24,29 @@ namespace GerenciamentoTarefasJBS.Controllers
         {
             return await _context.Users.ToListAsync();
         }
+
+        // POST: api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        // GET: api/users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
     }
 }
--- backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Models/User.cs
+++ backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Models/User.cs
@@ -7,6 +7,9 @@ namespace GerenciamentoTarefasJBS.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public List<Task> Tasks { get; set; }
+
+        //inicializando Tasks com uma nova lista vazia.Isso garante que, se não recebermos nenhuma lista de tarefas
+        //na criação de um usuário, Tasks será apenas uma lista vazia
+        public List<Task> Tasks { get; set; } = new List<Task>();
     }
 }
