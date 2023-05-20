806c52bfad2a6e2c4dc1632efbbea554ba643516
Alan <alanbritodossantos2015@gmail.com>
2023-05-19 22:31:13 -0300

    .

--- backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Controllers/TasksController.cs
+++ backend/GerenciamentoTarefasJBS/GerenciamentoTarefasJBS/Controllers/TasksController.cs
@@ -4,6 +4,7 @@ using GerenciamentoTarefasJBS.Models;
 using GerenciamentoTarefasJBS.Data;
 using System.Linq;
 using Task = GerenciamentoTarefasJBS.Models.Task;
+using Microsoft.EntityFrameworkCore;
 
 namespace GerenciamentoTarefasJBS.Controllers
 {
@@ -44,5 +45,68 @@ namespace GerenciamentoTarefasJBS.Controllers
             // Se a tarefa foi encontrada, a retornamos
             return task;
         }
+
+        // POST: api/Tasks
+        [HttpPost]
+        public async Task<ActionResult<Task>> PostTask(Task task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(int id, Task task)
+        {
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Task>> DeleteTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return task;
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+
+
     }
 }
