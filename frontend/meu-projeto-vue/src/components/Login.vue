ac36690d9dbb6f0f15247cfdb2d3bb3157be28e7
Alan <alanbritodossantos2015@gmail.com>
2023-05-20 20:29:36 -0300

    foi melhorado o visual do login

--- frontend/meu-projeto-vue/src/components/Login.vue
+++ frontend/meu-projeto-vue/src/components/Login.vue
@@ -1,47 +1,48 @@
 <template>
-  <div>
-    <h2>Login</h2>
+  <div class="login">
     <form @submit.prevent="submitForm">
-      <div>
-        <label for="username">Username:</label>
-        <input id="username" v-model="loginData.username" type="text" required>
+      <h2>Login</h2>
+      <div class="input-group">
+        <label for="username">Username</label>
+        <input type="text" id="username" v-model="loginData.username" required />
       </div>
-      <div>
-        <label for="password">Password:</label>
-        <input id="password" v-model="loginData.password" type="password" required>
+      <div class="input-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="loginData.password" required />
       </div>
-      <div>
-        <button type="submit">Login</button>
-      </div>
+      <button type="submit">Login</button>
     </form>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'LoginPage',
+  name: "LoginPage",
   data() {
     return {
       loginData: {
-        username: '',
-        password: ''
-      }
-    }
+        username: "",
+        password: "",
+      },
+    };
   },
   methods: {
     async submitForm() {
       try {
-        const response = await axios.post('http://localhost:5010/users/login', this.loginData);
+        const response = await axios.post(
+          "http://localhost:5010/users/login",
+          this.loginData
+        );
 
         if (response.status === 200) {
           // Salvar o token de autenticação recebido em algum lugar
           // Isso poderia ser no local storage, por exemplo:
-          window.localStorage.setItem('authToken', response.data.token);
+          window.localStorage.setItem("authToken", response.data.token);
 
           // Redirecionar o usuário para a página de tarefas
-          this.$router.push('/tasks');
+          this.$router.push("/tasks");
         } else {
           // Tratar erros retornados pelo servidor
           console.error(response.data.message);
@@ -50,11 +51,47 @@ export default {
         // Tratar erros que ocorrerem ao fazer a requisição
         console.error(error);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-/* estilo */
+.login {
+  width: 300px;
+  margin: 0 auto;
+  padding: 2em;
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+  border-radius: 8px;
+}
+.login h2 {
+  text-align: center;
+  color: #2c3e50;
+}
+.input-group {
+  margin-bottom: 1em;
+}
+.input-group label {
+  display: block;
+  margin-bottom: 0.5em;
+}
+.input-group input {
+  width: 100%;
+  padding: 0.5em;
+  font-size: 0.9em;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+}
+button {
+  width: 100%;
+  padding: 0.7em;
+  background-color: #2c3e50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #1a242f;
+}
 </style>
