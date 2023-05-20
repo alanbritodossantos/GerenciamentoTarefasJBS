966d2582a925eafe7f9de338e4fef87f063deddb
Alan <alanbritodossantos2015@gmail.com>
2023-05-20 20:39:32 -0300

    criar a estrutura do componente no arquivo Register.vue

--- frontend/meu-projeto-vue/src/components/Register.vue
+++ frontend/meu-projeto-vue/src/components/Register.vue
@@ -1,18 +1,57 @@
 <template>
-  <div>
-    <h1>Bem-vindo à nossa aplicação de gerenciamento de tarefas!</h1>
-    <p>Esta é a página Register.</p>
+  <div class="register">
+    <form @submit.prevent="submitForm">
+      <h2>Register</h2>
+      <div class="input-group">
+        <label for="username">Username</label>
+        <input type="text" id="username" v-model="registerData.username" required />
+      </div>
+      <div class="input-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="registerData.password" required />
+      </div>
+      <button type="submit">Register</button>
+    </form>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'RegisterPage',
-}
+  name: "RegisterPage",
+  data() {
+    return {
+      registerData: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const response = await axios.post(
+          "http://localhost:5010/users/register",
+          this.registerData
+        );
+
+        if (response.status === 200) {
+          // Redirecionar o usuário para a página de login
+          this.$router.push("/login");
+        } else {
+          // Tratar erros retornados pelo servidor
+          console.error(response.data.message);
+        }
+      } catch (error) {
+        // Tratar erros que ocorrerem ao fazer a requisição
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-h1 {
-  color: #2c3e50;
-}
+/* A estilização será similar à do login */
 </style>
